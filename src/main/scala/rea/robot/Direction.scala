28be1eee00279e4b68377f4d085412765629c769
michal pisanko <mpisanko@gmail.com>
2018-02-24 14:45:57 +1100

    remove unused function

--- src/main/scala/rea/robot/Direction.scala
+++ src/main/scala/rea/robot/Direction.scala
@@ -20,13 +20,3 @@ final case object West extends Direction {
   override def left: Direction = South
   override def right: Direction = North
 }
-
-object Direction {
-  def parse(dir: String): Option[Direction] = dir.trim.toUpperCase match {
-    case "NORTH" => Some(North)
-    case "SOUTH" => Some(South)
-    case "EAST"  => Some(East)
-    case "WEST"  => Some(West)
-    case _       => None
-  }
-}
--- src/test/scala/rea/robot/DirectionTest.scala
+++ src/test/scala/rea/robot/DirectionTest.scala
@@ -4,20 +4,6 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class DirectionTest extends FlatSpec with Matchers {
 
-  it should "parse direction from string regardless of case and whitespace" in {
-    Direction.parse("north") shouldEqual Some(North)
-    Direction.parse("South") shouldEqual Some(South)
-    Direction.parse("     eAsT") shouldEqual Some(East)
-    Direction.parse(" WESt    ") shouldEqual Some(West)
-  }
-
-  it should "ignore bogus entries" in {
-    Direction.parse("") shouldEqual None
-    Direction.parse("   ") shouldEqual None
-    Direction.parse("not a direction") shouldEqual None
-    Direction.parse("southeast") shouldEqual None
-  }
-
   it should "get back to self after four rotations the same way" in {
     Seq(North, East, South, West).foreach(d => d.left.left.left.left shouldEqual d)
     Seq(North, East, South, West).foreach(d => d.right.right.right.right shouldEqual d)
